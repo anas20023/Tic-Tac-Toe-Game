f5138785af1c94c37b15e3629cc4817e9cf1d174
anas20023 <yorder07@gmail.com>
2023-12-10 04:33:45 +0600

    Remove color styling from button

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
     draw++;
     if (turn) {
-      btn.style.color = "#1b4332";
       btn.textContent = p_1;
       turn = !turn;
     } else {
